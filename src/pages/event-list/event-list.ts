c9d71a37f4670c528c8d7f17ceb8b99db6fa6fbd
Aplikom Inixindo <aplikominixindo@gmail.com>
2018-04-25 15:06:19 +0700

    commit this new file

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the EventListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { EventProvider } from '../../providers/event/event';
 
 @IonicPage()
 @Component({
@@ -14,12 +8,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'event-list.html',
 })
 export class EventListPage {
+  public eventList: Array<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public eventProvider: EventProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventListPage');
+
+    // baca eventList dari firebase
+    this.eventProvider.getEventList()
+      .on('value', eventListSnapshot => {
+        this.eventList = [];  // array
+        eventListSnapshot.forEach(snap => {
+          this.eventList.push({
+            id: snap.key,     // membedakan antar event
+            name: snap.val().name,
+            date: snap.val().date,
+            price: snap.val().price,
+            contact: snap.val().contact
+          });
+          return false;
+        });
+      });
+  }
+
+  // melihat detail salah satu event
+  goToDetail(eventId): void {
+    this.navCtrl.push('EventDetailPage', { eventId: eventId });
   }
 
 }
